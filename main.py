311fff88cbc8becaf6da21a424a8c702b071b2a2
ArusyakAnna <88579003+ArusyakAnna@users.noreply.github.com>
2021-08-11 16:40:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -78,14 +78,14 @@
 # print((s2-1),(s1-1))
 
 #Knight’s Possible Moves
-list_1 = [1, 2, 3, 4, 5]
-list_2 = [25, 56, 98, 36]
+# list_1 = [1, 2, 3, 4, 5]
+# list_2 = [25, 56, 98, 36]
 
-list_5 = list_1 + list_2
-print(list_5)
-list_1.append(list_2)
-list_1.extend(list_2)
-print('=' * 100)
-print(list_2)
-print(list_1)
+# list_5 = list_1 + list_2
+# print(list_5)
+# list_1.append(list_2)
+# list_1.extend(list_2)
+# print('=' * 100)
+# print(list_2)
+# print(list_1)
 
