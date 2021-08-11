cd1394dff33fe41d479444de57e613d195d2d96c
Arusyak Babayan <Arusyak.Babayan@acba.am>
2021-08-11 09:53:25 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# Digit Sum 4
+
+
+# num = int(input())
+# digit_1 = num // 1000
+# two_digit_num = num % 1000 #511
+# digit_2 = two_digit_num // 100 #5.11
+# three_digit_num = two_digit_num % 100
+# digit_3 = three_digit_num // 10
+# digit_4 = two_digit_num % 10
+# print(digit_1 + digit_2 + digit_3 + digit_4)
+
+
+# Area of a right triangle
+
+
+# a = int(input())
+# b = int(input())
+# s = ((a * b) /2)
+# print(s)
+
+# Arithmetic Progression
+
+# a1 = int(input())
+# a2 = int(input())
+# n = int(input())
+#
+# d = a2 - a1
+# an = a1 + d*(n-1)
+# print (an)
+
+# Century from year
+
+# year = int(input())
+# century = 1 + (year - 1) // 100
+# print(century)
+
+
+
+# Knight’s Possible Moves
+
+
+#Strings 1
+# string = str(input())
+#
+# x = string[len(string)//2 - 1]
+# y = string[len(string)//2]
+# z = string[len(string)//2 + 1]
+#
+# print (x + y +z)
+
+#Strings 2
+
+# s1 = str(input())
+# s2 = str(input())
+#
+# first_part = s1[0:(len(s1)//2)]
+# last_part = s1[len(s1)//2:len(s1)]
+# print(first_part + s2 + last_part)
+
+
+#String 3
+
+# s1 = str(input())
+# s2 = str(input())
+#
+# first_part = s1[0:1]+s2[0:1]
+# middle_part = s1[len(s1)//2] + s2[len(s2)//2]
+# last_part = s1[-1] + s2[-1]
+# print(first_part + middle_part + last_part)
+
+
+# Two men
+
+# s1 = int(input())
+# s2 = int(input())
+#
+# print((s2-1),(s1-1))
+
+#Knight’s Possible Moves
+list_1 = [1, 2, 3, 4, 5]
+list_2 = [25, 56, 98, 36]
+
+list_5 = list_1 + list_2
+print(list_5)
+list_1.append(list_2)
+list_1.extend(list_2)
+print('=' * 100)
+print(list_2)
+print(list_1)
+
