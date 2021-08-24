02937ede08ec45945f7c023098121cc21c95d173
ArusyakAnna <arusyakbabayan7@gmail.com>
2021-08-24 18:18:27 +0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Homework #1.py

--- Homework #3.py
+++ Homework #3.py
@@ -1,14 +1,32 @@
 # Homework 3
 # Salaries
 
-def max_min(lst):
-    lst.sort(reverse=True)
-    return lst[0] - lst[-1]
+# def max_min(lst):
+#     lst.sort(reverse=True)
+#     diff = lst[0] - lst[-1]
+#     return diff
+#
+# lst = []
+# n = int(input())
+# for i in range(0, n):
+#     el = int(input())
+#     lst.append(el)
+#
+# print(max_min(lst))
 
-lst = []
-n = int(input())
-for i in range(0, n):
-    el = int(input())
-    lst.append(el)
+# Boring Numbers
 
-print(max_min(lst))
+def dig_convert(num):
+    digit_string = str(num)
+    digit_map = map(int, digit_string)
+    digit_list = list(digit_map)
+    return digit_list
+
+
+List = dig_convert(num=)
+# Result from count matches with result from len()
+result = List.count(List[0]) == len(List)
+if (result):
+   print("Boring")
+else:
+   print("Interesting")
